a5470dab61f1a2165e7b77951add806705467f63
volodya <volodya@intelex.ca>
2015-10-08 12:23:46 +0300

    event list

--- js/app.js
+++ js/app.js
@@ -1,5 +1,24 @@
 var app = angular.module ('eventApp', [] );
 
+app.directive("eventNav", function() { 
+    return {
+      restrict: 'E',
+      templateUrl: "template/event-nav.html",
+      controller: function() {
+          this.link = 1;
+
+          this.isLink = function(checkLink) {
+            return this.link === checkLink;
+          };
+
+          this.setLink = function(activeLink) {
+            this.link = activeLink;
+          };
+        },
+        controllerAs: "link"
+    };
+  });
+
 app.directive("eventMap", function() {
     return {
       restrict: 'E',
@@ -27,3 +46,11 @@ app.directive("eventFind", function() {
       templateUrl: "template/event-find.html"
     };
   });
+
+app.controller('EventController', ['$http',function($http){
+    var eventlist = this;
+    eventlist.events = [];
+    $http.get('data/events.json').success(function(data){
+    eventlist.events = data;
+  });
+  }]);
