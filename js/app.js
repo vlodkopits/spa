e29d8b84aeb9e63efe2b2ee6cb2cf96e571ceae0
volodya <volodya@intelex.ca>
2015-10-21 16:37:02 +0300

    single event

--- js/app.js
+++ js/app.js
@@ -64,6 +64,21 @@ eventApp.controller('SingleEventCtrl', ['$scope', '$routeParams', '$filter',
   function($scope, $routeParams, $filter) {
     $scope.eventId = $routeParams.eventId;
     $scope.event_single = $filter('filter')(eventData, function (d) {return d.id === $routeParams.eventId;})[0];
+
+    var mapOptions = {
+        zoom: 13,
+        center: new google.maps.LatLng($scope.event_single.lat, $scope.event_single.lng),
+        mapTypeId: google.maps.MapTypeId.ROADMAP
+    }
+    
+    var marker = new google.maps.Marker({
+        position: { lat: 12.97, lng: 77.59 },
+        map: event-gmap,
+        title: $scope.event_single.location
+      });
+
+    $scope.map = new google.maps.Map(document.getElementById('event-gmap'), mapOptions);
+
   }]);
 
 // event add 
@@ -109,10 +124,10 @@ eventApp.controller ('EventMapCtrl',function ($scope){
             position: new google.maps.LatLng(info.lat, info.lng),
             title: info.title
         });
-        marker.content = '<div class="infoWindowContent"><img src="'+ info.image +'" alt="" class="fl mr10 w100px" />' + info.title + info.description + '</div>';
+        marker.content = '<div class="infoWindowContent"><img src="'+ info.image +'" alt="" class="fl mr10 w100px" />' + info.title  + info.description + '</div>';
         
         google.maps.event.addListener(marker, 'click', function(){
-            infoWindow.setContent('<h2>' + marker.title + '</h2>' + marker.content);
+            infoWindow.setContent('<h2><a href="#event/'+info.id+'">' + marker.title + '</a></h2>' + marker.content);
             infoWindow.open($scope.map, marker);
         });
         
