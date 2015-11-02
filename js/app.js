342b675b1c734b42b37bcf7050f937e115da15c8
volodya <volodya@intelex.ca>
2015-11-02 14:24:11 +0200

    home page

--- js/app.js
+++ js/app.js
@@ -149,7 +149,7 @@ eventApp.config(function($routeProvider) {
     $routeProvider
 
         // route for the map page
-        .when('/', {
+        .when('/map', {
             templateUrl : 'template/event-map.html',
             controller  : 'EventMapCtrl'
         })
@@ -172,7 +172,7 @@ eventApp.config(function($routeProvider) {
             controller  : 'SingleEventCtrl'
         }).
         otherwise({
-        redirectTo: '/'
+        redirectTo: '/map'
       });
 
 });
