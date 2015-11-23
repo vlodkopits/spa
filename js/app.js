04febd6df39bbdefd55ae30370ab1d7222d270e4
volodya <volodya@intelex.ca>
2015-11-23 17:32:01 +0200

    some

--- js/app.js
+++ js/app.js
@@ -245,6 +245,32 @@ eventApp.controller('EventListCtrl', function ($scope) {
     $scope.events = [];
     $scope.events = actualEvents;
     $scope.currDate = moment(currDate).format("YYYY-MM-DD");
+
+    $(function(){
+
+      $(".filter_cat_all").click(function(){      
+        $("button.btn").removeClass("active disabled");
+        $(this).addClass("active disabled");
+        $(".link_cat_all").show();
+      });
+
+      function FilterCat(i) {
+        $(".filter_cat_" + i).click(function(){
+          $("button.btn").removeClass("active disabled");
+          $(this).addClass("active disabled");
+            $(".link_cat_all").hide();
+            $(".link_cat_" + i).show();
+        });
+      }
+
+      cat_1 = new FilterCat(1);
+      cat_2 = new FilterCat(2);
+      cat_3 = new FilterCat(3);
+      cat_4 = new FilterCat(4);
+      cat_5 = new FilterCat(5);
+      cat_6 = new FilterCat(6);
+      cat_7 = new FilterCat(7);
+    });
 });
 
 // event single 
