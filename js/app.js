0477d775d21fe2fddeeebaeb8c003b0002e9f983
volodya <volodya@intelex.ca>
2015-11-27 18:08:51 +0200

    add fav, fb

--- js/app.js
+++ js/app.js
@@ -528,8 +528,8 @@ eventApp.controller('AdminEventCtrl', function ($scope) {
 });
 
 // login 
-eventApp.controller('LoginEventCtrl', function () {
-    // This is called with the results from from FB.getLoginStatus().
+eventApp.controller('LoginEventCtrl', function ($scope) {
+  // This is called with the results from from FB.getLoginStatus().
   function statusChangeCallback(response) {
     console.log('statusChangeCallback');
     console.log(response);
@@ -550,6 +550,15 @@ eventApp.controller('LoginEventCtrl', function () {
       document.getElementById('status').innerHTML = 'Please log ' +
         'into Facebook.';
     }
+  }
+
+  // This function is called when someone finishes with the Login
+  // Button.  See the onlogin handler attached to it in the sample
+  // code below.
+  function checkLoginState() {
+    FB.getLoginStatus(function(response) {
+      statusChangeCallback(response);
+    });
   }
 
   window.fbAsyncInit = function() {
@@ -588,29 +597,17 @@ eventApp.controller('LoginEventCtrl', function () {
     fjs.parentNode.insertBefore(js, fjs);
   }(document, 'script', 'facebook-jssdk'));
 
-  // login
-  function login() {
-    FB.login(function(response) {
-        if (response.authResponse) {
-            // connected
-        testAPI();
-        } else {
-            // cancelled
-        }
-    }, { scope: 'email' });
-  }
   // Here we run a very simple test of the Graph API after login is
   // successful.  See statusChangeCallback() for when this call is made.
   function testAPI() {
+    console.log('Welcome!  Fetching your information.... ');
     FB.api('/me', function(response) {
-      console.log(JSON.stringify(response));
-      console.log('name: '+ response.name);
-      console.log('id: '+ response.id);
-      console.log('email: '+ response.email);
       console.log('Successful login for: ' + response.name);
       document.getElementById('status').innerHTML =
         'Thanks for logging in, ' + response.name + '!';
     });
   }
 
+  $scope.showMyEvents = [];
+  
 });
