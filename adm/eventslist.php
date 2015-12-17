0f59de0b7cf64d944e1b54cfe1d6491370cbfb5d
vlodkopits <vlodkopits@gmail.com>
2015-12-17 20:47:23 +0200

    oauth del

--- adm/eventslist.php
+++ adm/eventslist.php
@@ -1,5 +1,8 @@
 <? 
 //header('Content-Type: application/json');
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Headers: Cache-Control, Pragma, Origin, Authorization, Content-Type, X-Requested-With');
+header('Access-Control-Allow-Methods: GET, PUT, POST');
 echo "[";
 include 'config.php'; 
 
@@ -35,22 +38,22 @@ $enable=$row['enable'];
         //json single event
         $events[] = '
     	{
-    	"id": "'.$row['id'].'",
-            "image": "data\/posters\/'.$event_img.'",
-            "title": '.json_encode($row['title']).',
-            "category": "'.$row['category'].'",
-            "description": '.json_encode($row['description']).',
-            "dates": ['.$row['dates'].'],
-            "tickets": '.json_encode($row['tickets']).',
-            "tickets_link": '.json_encode($row['tickets_link']).',
-            "location": '.json_encode($row['location']).',
-            "location_addr": '.json_encode($row['location_addr']).',
-            "lat": "'.$row['lat'].'",
-            "lng": "'.$row['lng'].'",
-            "web": "'.$row['web'].'",
-            "email": "'.$row['email'].'",
-            "phone": "'.$row['phone'].'",
-            "enable": "'.$row['enable'].'"
+    	id: '.$row['id'].',
+        "image": "data\/posters\/'.$event_img.'",
+        "title": '.json_encode($row['title']).',
+        "category": "'.$row['category'].'",
+        "description": '.json_encode($row['description']).',
+        "dates": ['.$row['dates'].'],
+        "tickets": '.json_encode($row['tickets']).',
+        "tickets_link": '.json_encode($row['tickets_link']).',
+        "location": '.json_encode($row['location']).',
+        "location_addr": '.json_encode($row['location_addr']).',
+        "lat": "'.$row['lat'].'",
+        "lng": "'.$row['lng'].'",
+        "web": "'.$row['web'].'",
+        "email": "'.$row['email'].'",
+        "phone": "'.$row['phone'].'",
+        "enable": "'.$row['enable'].'"
     	}
     	';
 	
